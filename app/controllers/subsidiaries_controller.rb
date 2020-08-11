b3a543dd4ceb8a7763a06d1c1c54c7331d585f1c
Grégory <gregory_735@hotmail.com>
2020-08-11 17:04:16 -0300

    [Admin] Cadastrar uma filial

--- app/controllers/subsidiaries_controller.rb
+++ app/controllers/subsidiaries_controller.rb
@@ -7,4 +7,19 @@ class SubsidiariesController < ApplicationController
         @Subsidiary = Subsidiary.find(params[:id]) # params é um hash
     end
 
+    def new
+        @subsidiary = Subsidiary.new
+    end
+
+    def create
+        @subsidiary = Subsidiary.create(subsidiary_params)
+        redirect_to @subsidiary
+        #rails já entende que redirecionar para esse objeto é 
+        #fazer um show dele, e o rails já cria o path certo.
+    end
+
+    private
+        def subsidiary_params
+            params.require(:subsidiary).permit(:name, :cnpj, :address)
+        end
 end
---
+++ spec/features/admin_register_subsidiary_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+
+feature 'admin register subsidiary' do
+    scenario 'from index page' do
+        visit root_path
+        click_on 'Filiais'
+
+        expect(page).to have_link('Registrar uma nova filial',
+                    href: new_subsidiary_path)
+    end
+
+    scenario 'successfully' do 
+        visit root_path
+        click_on 'Filiais'
+        click_on 'Registrar uma nova filial'
+
+        fill_in 'Nome', with: 'Maria'
+        fill_in 'CNPJ', with: '00-000.000/0001-51'
+        fill_in 'Endereço', with: 'Rua xxxxxxxx xxxx'
+
+        click_on 'Enviar'
+
+        expect(current_path).to eq subsidiary_path(Subsidiary.last)
+        expect(page).to have_content('Maria')
+        expect(page).to have_content('00-000.000/0001-51')
+        expect(page).to have_content('Rua xxxxxxxx xxxx')
+        expect(page).to have_link('Voltar')
+    end
+end
